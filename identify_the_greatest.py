fbf447f8fade5414d619e21f7b3061189dafdf4c
Zander2895 <jacquesalexander71@gmail.com>
2024-04-20 14:15:28 -0400

    first commit

---
+++ identify_the_greatest.py
@@ -0,0 +1,13 @@
+...
+#Write a Python program that prompts the user to enter three numbers.
+#The program should then identify and print out the largest number among the three.
+...
+
+greatest_num = int(input("Enter three numbers:"))
+
+if greatest_num <= 3:
+    print("This number is the smallest.")
+elif greatest_num == 3:
+    print("The number is the midddle numer.")
+elif greatest_num >= 4:
+    print("The number is the greatest.")
---
+++ identify_the_smallest.py
@@ -0,0 +1,13 @@
+...
+#Extend your program from Task 1 to also determine
+#the smallest number among the three and print it out.
+...
+
+smallest_num = int(input("Enter three numbers:"))
+
+if smallest_num <= 3:
+    print("This number is the smallest.")
+elif smallest_num == 3:
+    print("The number is the midddle numer.")
+elif smallest_num >= 4:
+    print("The number is the greatest.")
